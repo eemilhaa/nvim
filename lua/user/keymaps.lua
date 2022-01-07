ac22fc691f0fcc63772ef60bfab9032dd33a38b4
eemilhaa <eemil.haapanen@gmail.com>
2022-01-07 18:20:14 +0200

    edit keys

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -18,13 +18,13 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
----- Normal --
----- Better window navigation
+-- Normal --
+-- Better window navigation
 --keymap("n", "<C-h>", "<C-w>h", opts)
 --keymap("n", "<C-j>", "<C-w>j", opts)
 --keymap("n", "<C-k>", "<C-w>k", opts)
 --keymap("n", "<C-l>", "<C-w>l", opts)
---
+
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 --
 -- Resize windows with arrows
