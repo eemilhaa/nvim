1975dae26797396d4c22f59bdc3f706260602391
eemilhaa <eemil.haapanen@gmail.com>
2022-01-24 11:13:35 +0200

    colors

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,16 +1,17 @@
--- Customize theme here
-require("onedark").setup {
-    style = "warm",
-    term_colors = true
-}
-
-
--- Set theme here
+-- Select theme here
 local colorscheme = "onedark"
 
--- Use protected call
+-- Theme-specific setups here
+if colorscheme == "onedark" then
+    require("onedark").setup {
+        style = "warm",
+        term_colors = true
+    }
+end
+
+-- Set theme here, use protected call
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
 end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  --use 'folke/tokyonight.nvim'
+  use 'folke/tokyonight.nvim'
   use 'navarasu/onedark.nvim'
 
 
