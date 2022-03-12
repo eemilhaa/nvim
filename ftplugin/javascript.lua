642478b284a86acb2754bb715d2778f51f2a1124
eemilhaa <eemil.haapanen@gmail.com>
2022-03-12 20:44:53 +0200

    update

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,3 +1,3 @@
-
+-- 2 spaces for js
 vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2                             -- insert spaces for a tab
