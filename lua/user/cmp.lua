4ab64f43b5b21840eeca7182733dedcb527cdd69
eemilhaa <eemil.haapanen@gmail.com>
2022-03-13 13:01:21 +0200

    diable tab completion

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -26,24 +26,25 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = false, -- fixes some python shenanigans
         },
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-            else
-                fallback()
-            end
-        end,
+        -- Use tab at your own risk (causes some weird shit sometimes)
+--        ['<Tab>'] = function(fallback)
+--            if cmp.visible() then
+--                cmp.select_next_item()
+--            elseif luasnip.expand_or_jumpable() then
+--                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+--            else
+--                fallback()
+--            end
+--        end,
+--        ['<S-Tab>'] = function(fallback)
+--            if cmp.visible() then
+--                cmp.select_prev_item()
+--            elseif luasnip.jumpable(-1) then
+--                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+--            else
+--                fallback()
+--            end
+--        end,
     },
     sources = {
         -- the order matters here, place most important first
