15325c0c4f3f994b709a8406c621478c6055431a
eemilhaa <eemil.haapanen@gmail.com>
2022-03-14 11:10:01 +0200

    cleanup

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,7 +1,7 @@
 -- good example config: https://github.com/neovim/nvim-lspconfig/wiki/Snippets
 -- docs: https://github.com/hrsh7th/nvim-cmp#setup
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+local cmp = require "cmp"
+local luasnip = require "luasnip"
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
@@ -12,46 +12,27 @@ luasnip.filetype_extend("javascript", { "html" })
 cmp.setup {
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
         end,
     },
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
-            select = false, -- fixes some python shenanigans
+            select = false,
         },
-        -- Use tab at your own risk (causes some weird shit sometimes)
---        ['<Tab>'] = function(fallback)
---            if cmp.visible() then
---                cmp.select_next_item()
---            elseif luasnip.expand_or_jumpable() then
---                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
---            else
---                fallback()
---            end
---        end,
---        ['<S-Tab>'] = function(fallback)
---            if cmp.visible() then
---                cmp.select_prev_item()
---            elseif luasnip.jumpable(-1) then
---                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
---            else
---                fallback()
---            end
---        end,
     },
     sources = {
         -- the order matters here, place most important first
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
     },
 }
 
