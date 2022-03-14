3cf815ced7092d8d6939ff821c490bcca524b5f5
eemilhaa <eemil.haapanen@gmail.com>
2022-03-14 11:56:43 +0200

    add comments

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,13 +3,14 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
--- Virtual line navigation
+-- Navigate wrapped lines like you would normal lines
 keymap("n", "j", "gj", opts)
 keymap("n", "k", "gk", opts)
 keymap("n", "0", "g0", opts)
 keymap("n", "$", "g$", opts)
 keymap("n", "I", "gI", opts)
 keymap("n", "A", "g$a", opts)
+-- Same for visual mode
 keymap("v", "j", "gj", opts)
 keymap("v", "k", "gk", opts)
 
