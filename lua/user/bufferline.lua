78f384796385651c99fcff0abb011b87a5640508
eemilhaa <eemil.haapanen@gmail.com>
2022-04-11 19:08:52 +0300

    offset bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -2,5 +2,12 @@ require('bufferline').setup {
     options = {
         show_buffer_close_icons = false,
         show_close_icon = false,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "",
+                padding = 0
+            }
+        },
     }
 }
