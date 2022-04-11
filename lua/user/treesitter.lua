94cb5cc6dcf87ec2845d42e9f262622dcd52480c
eemilhaa <eemil.haapanen@gmail.com>
2022-04-11 15:16:04 +0300

    update treesitter config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,9 +1,6 @@
-local configs = require("nvim-treesitter.configs")
-
-configs.setup {
-    ensure_installed = "maintained",
+require("nvim-treesitter.configs").setup {
+    ensure_installed = { "lua", "javascript", "python" },
     sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "" }, -- list of language that will be disabled
