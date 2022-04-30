1aae15fd57bab3dde373c598e650f57affd2e173
eemilhaa <eemil.haapanen@gmail.com>
2022-04-30 21:00:06 +0300

    use regex highlights

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ require("nvim-treesitter.configs").setup {
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "" }, -- list of language that will be disabled
-        --additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = true,
     },
     indent = { enable = true, disable = { "javascript" } },
 }
