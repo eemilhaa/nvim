8720b49949fbfb227282f792e20add8f0ace5f1a
eemilhaa <eemil.haapanen@gmail.com>
2022-05-01 12:38:53 +0300

    lsp: ditch deprecated on_server_ready

---
+++ lua/user/lsp/lsp-configs.lua
@@ -0,0 +1,17 @@
+local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require("lspconfig")
+local handlers = require("user.lsp.handlers")
+
+local servers = { "pylsp", "sumneko_lua", "html", "tsserver" }
+
+lsp_installer.setup {
+    ensure_installed = servers
+}
+
+for _, server in pairs(servers) do
+    local opts = {
+        on_attach = handlers.on_attach,
+        capabilities = handlers.capabilities,
+    }
+    lspconfig[server].setup(opts)
+end
