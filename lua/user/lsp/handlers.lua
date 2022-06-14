08c99fa34d48c68a07290a40a9ab34e2be4f6827
eemilhaa <eemil.haapanen@gmail.com>
2022-06-14 10:45:31 +0300

    update handlers

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -42,7 +42,7 @@ M.on_attach = function(client, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -54,7 +54,7 @@ M.on_attach = function(client, bufnr)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<leader>bf', vim.lsp.buf.formatting, bufopts) -- TODO
+    vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format, bufopts) -- TODO
 
     -- LSP illumination
     require('illuminate').on_attach(client)
